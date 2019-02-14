3a4d767de8e9132e69a13d43e731b1184631568d
timick <timikm2002@gmail.com>
2019-02-14 21:30:42 +0500

    В процессе написания алгоритма парсинга

--- parsers.py
+++ parsers.py
@@ -5,7 +5,7 @@ import requests
 # TODO парсер для ресурса http://moeobrazovanie.ru
 class MoeObr():
     def __init__(self):
-        self.BASE_URL = 'https://moeobrazovanie.ru/olimpiady.php?predmet={}&klass={}&data={}&sort=date'
+        self.BASE_URL = 'https://moeobrazovanie.ru/olimpiady.php?predmet={}&klass={}&data={}&sort=date&page={}'
         self.HEADERS = {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
@@ -13,8 +13,9 @@ class MoeObr():
         self.ITEMS_ON_PAGE = 20
     
     def getList(self, subject=-1, class_=-1, date=-1):
+        result = []
         # make url for this query
-        url = self.BASE_URL.format(subject, class_, date)
+        url = self.BASE_URL.format(subject, class_, date, 1)
 
         # make request
         session = requests.Session()
@@ -28,13 +29,33 @@ class MoeObr():
             amount = int(soup.find('b', class_='ml10').text)
             if amount == 0:
                 return '0'
-
             # calculate number of pages
             pages_amount = amount // self.ITEMS_ON_PAGE
             if amount % self.ITEMS_ON_PAGE > 0:
                 pages_amount += 1
+
+            raw_elements = soup.find_all('div', class_='OSROlyRow')
+            for i in range(len(raw_elements)):
+                result.append(self.getDictFromData(raw_elements[i]))
+            
+            print(result)
+            
         else:
             return 'error'
+    
+    def getDictFromData(self, data):
+        result = {
+            'title': None,
+            'deadlines': None,
+            'orgs': None,
+            'subjects': [],
+            'classes': []
+        }
+        # fetch a title
+        title = data.find('a', class_='olyTtl').text
+        #fetch subjects
+        regions = data.find()
+        return result
 
 
 # TODO парсер для ресурса http://olimpiada.ru
@@ -68,11 +89,5 @@ class OlimpiadaRu():
             'Technology': '%5B17%5D',
             'Psychology': '%5B28%5D'
         }
-    def getOlympiadsList(self, type_='any', subjects=[], period='year', period_date='', class_='any'):
-        url = self.URL + 'type={}&class={}&period={}&period_date={}'.format(type_, class_, period, period_date)
-        content = requests.get(url).text
-        soup = BeautifulSoup(content, 'html.parser')
-
-        numberOfEvents = int(soup.find(id="megatitle").text.split()[0])
-        count = soup.find_all("div", class_="fav_olimp")
-        print(len(count))
+    def getList(self, type_='any', subjects=[], period='year', period_date='', class_='any'):
+        pass
