3d080d348be6682e6bc38b687952982bef755d53
timick <timikm2002@gmail.com>
2019-02-20 19:45:27 +0500

    some changes

--- parsers.py
+++ parsers.py
@@ -5,12 +5,15 @@ import requests
 # TODO парсер для ресурса http://moeobrazovanie.ru
 class MoeObr():
     def __init__(self):
+        self.SERVER = 'https://moeobrazovanie.ru'
         self.BASE_URL = 'https://moeobrazovanie.ru/olimpiady.php?predmet={}&klass={}&data={}&sort=date&page={}'
-        self.HEADERS = {
+        self.headers = {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
         }
         self.ITEMS_ON_PAGE = 20
+        self.info_table_rows = ['scale', 'level', 'type', 'classes', 'deadlines', 'subjects', 'places', 'rounds', 
+        'awardee_def', 'privileges', 'registration', 'site', 'orgs']
     
     def getList(self, subject=-1, class_=-1, date=-1):
         result = []
@@ -19,7 +22,7 @@ class MoeObr():
 
         # make request
         session = requests.Session()
-        request = session.get(url, headers=self.HEADERS)
+        request = session.get(url, headers=self.headers)
 
         if request.status_code == 200:
             # if success, parse the result
@@ -44,17 +47,29 @@ class MoeObr():
             return 'error'
     
     def getDictFromData(self, data):
+        # get info page
+        page_url = self.SERVER + data.find('a')['href']
+        request = requests.get(page_url, headers=self.headers)
+        if request.status_code != 200:
+            return 'error'
+        info_page = request.content
+
+        # forming output data
         result = {
-            'title': None,
-            'deadlines': None,
-            'orgs': None,
+            'title': 'undefined',
+            'deadlines': 'undefined',
+            'orgs': 'undefined',
             'subjects': [],
-            'classes': []
+            'classes': [],
+            'places': 'undefined'
         }
+
         # fetch a title
-        title = data.find('a', class_='olyTtl').text
-        #fetch subjects
-        regions = data.find()
+        result['title'] = data.find('a', class_='olyTtl').text
+
+        #fetch information from its page
+        oly_table_rows = info_page.find('table', class_='oly_table').find_all('tr')
+
         return result
 
 
