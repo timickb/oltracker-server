f0af25b6e48caf11b07eb55ba891939ab95f4848
timick <timikm2002@gmail.com>
2019-03-10 19:46:31 +0500

    fixed

--- main.py
+++ main.py
@@ -24,6 +24,37 @@ with open('matches.yml', 'r', encoding='utf-8') as file:
 
 # --important functions--
 
+def find_data_next(schclass, subject, oltype):
+    result = []
+    try:
+        schclass = int(schclass)
+    except:
+        schclass = -1
+    try:
+        subject = matches['subjects'][subject]
+    except:
+        subject = -1
+    
+    if oltype not in matches['types-olru']:
+        oltype = -1
+    
+    if (schclass not in range(5, 12)) and schclass != -1:
+        return result
+    
+    db = open('database_next.json', 'r', encoding='utf-8')
+    data = json.load(db)
+
+    for item in data:
+        isClass = False
+        try:
+            isClass = schclass in item['classes'] or schclass == -1
+        except:
+            isClass = False
+        if (isClass) and (subject in item['subjects'] or subject == -1):
+            result.append(item)
+
+    return result
+
 def find_data_current(schclass, subject, oltype):
     result = []
     try:
@@ -55,28 +86,6 @@ def find_data_current(schclass, subject, oltype):
 
     return result
 
-def find_data(class_, subject, date):
-    result = []
-    if class_ == -1: class_ = None
-    if subject == -1: subject = None
-    if date == -1: date = None
-
-    try:
-        subject = matches['subjects-mo'][subject]
-    except:
-        subject = None
-    
-    try:
-        date = matches['months'][date]
-    except:
-        date = None
-
-    db = open('database_next.json', 'r', encoding='utf-8')
-    data = json.load(db)
-    for item in data:
-        if (class_ in item['classes'] or class_ == None) and (subject in item['subjects'] or subject == None) and (date == item['date_start'] or date == None):
-            result.append(item)
-    return result
 
 # --HTTP server--
 
@@ -87,12 +96,12 @@ def info():
     html = open('index.html', 'r', encoding='utf-8').read()
     return html
 
-@app.route('/get')
+@app.route('/getNext')
 def get():
-    class_ = request.args.get('class')
+    schclass = request.args.get('class')
     subject = request.args.get('subject')
-    date = request.args.get('date')
-    return jsonify(find_data(class_, subject, date))
+    oltype = request.args.get('type')
+    return jsonify(find_data_next(schclass, subject, oltype))
 
 @app.route('/getCurrent')
 def getCurrent():
