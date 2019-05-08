2954cae7cbf3cd91d8d220550128906f7fb6a103
timickb <timikm2002@gmail.com>
2019-05-08 22:53:31 +0500

    Пофиксил

--- main.py
+++ main.py
@@ -139,7 +139,7 @@ def catch_all(path):
 @app.route('/updateUser', methods=['POST'])
 def updateUser():
     data = request.get_json()
-    logging.info("Received data: " + data)
+    logging.info("Received data: " + json.dumps(data))
     key = data['key']
     token = data['token']
     subject = data['subject']
